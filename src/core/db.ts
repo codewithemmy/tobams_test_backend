3a378c4bb46f5debf380d80a36416754cae22aba
codewithemmy <codewithemmy@gmail.com>
2024-01-18 14:05:04 +0100

    new changes

--- src/core/db.ts
+++ src/core/db.ts
@@ -3,10 +3,7 @@ import mongoose, { ConnectOptions } from "mongoose";
 const connectToDatabase = async () => {
   mongoose.set("strictQuery", false);
 
-  mongoose.connect(`${process.env.MONGO_URL!}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  } as ConnectOptions);
+  mongoose.connect(`${process.env.MONGO_URL!}`);
 
   const conn = mongoose.connection.on("connected", () => {
     console.log("Database Connected");
